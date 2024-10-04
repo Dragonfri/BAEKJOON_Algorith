7dc58eaeaeb180c11720447938abefc2936cf367
Andrew Kim <37804008+Dragonfri@users.noreply.github.com>
2024-10-04 15:57:21 +0900

    [Bronze I] Title: 단어 공부, Time: 300 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,10 +1,13 @@
 c = input()
 
+# 한글자면 그냥 소문자인지 체크하고 대문자로 바꿔서 출력
 if len(c) == 1:
     if 'a' <= c <= 'z':
         c = chr(ord(c) - 32)
     print(c)
 else:
+    # 한글자 이상이면 소문자 대문자로 치환해주고
+    # dictionary 타입에 key 값은 입력받은 대문자, value는 등장 횟수
     dict = {}
     sorted_dic = {}
     for i in c:
@@ -14,7 +17,11 @@ else:
             dict[i] = dict[i] + 1
         else:
             dict[i] = 1
-
+    
+    # value 기준으로 정렬
+    # 만약 정렬된 배열의 길이가 1이면 (ex. aaaaa 같이 한가지 글자만 반복되는 형태라면) 바로 출력)
+    # 그 외의 경우에는 가장 높은 숫자가 2개 이상 존재하는지 확인
+    # 2개 이상 존재하는 경우 '?' 출력, 아니면 index 0 출력(정렬 되어있으니까)
     sorted_dic = sorted(dict, key=lambda x: dict[x], reverse=True)
     if len(sorted_dic) == 1:
         print(sorted_dic[0])
