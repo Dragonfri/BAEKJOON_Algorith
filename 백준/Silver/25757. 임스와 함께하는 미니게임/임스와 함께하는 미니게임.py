b2c2bdc58544a02e2a3b2faa749e565effd98f68
Andrew Kim <37804008+Dragonfri@users.noreply.github.com>
2024-10-16 15:07:06 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 2380 ms, Memory: 42388 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
@@ -0,0 +1,17 @@
+Y = 2
+F = 3
+O = 4
+
+num, play = input().split()
+num = int(num)
+s = set()
+for i in range(num):
+    tmp = input()
+    s.add(tmp)
+
+if play == "Y":
+    print(len(s))
+elif play == "F":
+    print(int(len(s) / (F - 1)))
+else:
+    print(int(len(s) / (O - 1)))
